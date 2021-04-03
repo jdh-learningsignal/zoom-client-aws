e4bb65d8b78d4345a6e58ea16813f133df68e887
정동훈 <jeongdonghun@jeongdonghun-ui-MacBookAir.local>
2021-04-03 22:26:33 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
-import {React, useState, useEffect} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { API } from 'aws-amplify';
-import { listTraffics } from './graphql/queries';
-import { createTraffic as createTrafficMutation} from './graphql/mutations';
 
 function App() {
-  useEffect(() => {}, []);
-
-  async function sendTraffic(state) {
-    if (!state) return;
-    await API.graphql({ 
-      query: createTrafficMutation, 
-      variables: { 
-        input: {
-          studentId: 'rapido300@hanyang.ac.kr', 
-          meetingId: '1000000000', 
-          state: state 
-        }
-      } 
-    });
-  }
-
-  const onClickButton = e => {
-    sendTraffic(e.target.value);
-  };
-
   return (
     <div className="App">
-      <h1>Traffic Test</h1>
-      <div style={{display:"flex", position:"fixed", top:"80%", left:"70%", right:"0"}}>
-        <button onClick={onClickButton}
-                placeholder="매우 어려움"
-                value="RED"
-                style={{backgroundColor:"red", width:"100px", height:"100px"}}
-        />
-        <button onClick={onClickButton}
-                placeholder="약간 어려움"
-                value="YELLOW"
-                style={{backgroundColor:"yellow", width:"100px", height:"100px"}}
-        />
-        <button onClick={onClickButton}
-                placeholder="너무 쉬움"
-                value="GREEN"
-                style={{backgroundColor:"green", width:"100px", height:"100px"}}
-        />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
     </div>
   );
 }
