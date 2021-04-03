715b21fc5e5d38c80de99978eeb3b20b5f47e78b
정동훈 <jeongdonghun@jeongdonghun-ui-MacBookAir.local>
2021-04-03 21:25:08 +0900

    add api interaction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { API } from 'aws-amplify';
@@ -8,24 +8,36 @@ import { createTraffic as createTrafficMutation} from './graphql/mutations';
 const initialTrafficState = { studentId: 'rapido300@hanyang.ac.kr', meetingId: '1000000000', state: ''}
 
 function App() {
-  const [traffics, setTraffics] = useState({});
   const [trafficData, setTrafficData] = useState(initialTrafficState);
+
+  useEffect(() => {}, []);
+
+  async function sendTraffic() {
+    if (!trafficData.name || !trafficData.description) return;
+    await API.graphql({ query: createTrafficMutation, variables: { input: trafficData } });
+    setTrafficData(initialTrafficState);
+  }
+
+  function onClickButton(e) {
+    setTrafficData({ ...trafficData, 'state': e.target.value});
+    sendTraffic();
+  }
 
   return (
     <div className="App">
       <h1>Traffic Test</h1>
       <div style={{display:"flex", position:"fixed", top:"80%", left:"70%", right:"0"}}>
-        <button onClick={e => {}}
+        <button onClick={onClickButton}
                 placeholder="매우 어려움"
                 value="RED"
                 style={{backgroundColor:"red", width:"100px", height:"100px"}}
         />
-        <button onClick={e => {}}
+        <button onClick={onClickButton}
                 placeholder="약간 어려움"
                 value="YELLOW"
                 style={{backgroundColor:"yellow", width:"100px", height:"100px"}}
         />
-        <button onClick={e => {}}
+        <button onClick={onClickButton}
                 placeholder="너무 쉬움"
                 value="GREEN"
                 style={{backgroundColor:"green", width:"100px", height:"100px"}}
