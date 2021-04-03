97c519b6e54d3c0d6fad208417dd4ec796973f0a
정동훈 <jeongdonghun@jeongdonghun-ui-MacBookAir.local>
2021-04-03 20:12:51 +0900

    mod init

--- src/App.js
+++ src/App.js
@@ -5,24 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Traffic Test</h1>
-      <div style={{display:"flex", position:"fixed", top:"80%", left:"70%", right:"0"}}>
-        <button onClick={}
-                placeholder="매우 어려움"
-                value="RED"
-                style={{backgroundColor:"red", width:"100px", height:"100px"}}
-        />
-        <button onClick={}
-                placeholder="약간 어려움"
-                value="YELLOW"
-                style={{backgroundColor:"yellow", width:"100px", height:"100px"}}
-        />
-        <button onClick={}
-                placeholder="너무 쉬움"
-                value="GREEN"
-                style={{backgroundColor:"green", width:"100px", height:"100px"}}
-        />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
     </div>
   );
 }
